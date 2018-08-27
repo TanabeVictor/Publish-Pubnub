ea53f50afbccbb7111081666ae118019868dea2c
Tanabe <tanabebr@gmail.com>
2018-08-27 16:20:33 -0300

    Inicio da Implementação de Publicar Rede

--- src/main/java/Views/Principal.java
+++ src/main/java/Views/Principal.java
@@ -40,6 +40,11 @@ public class Principal extends javax.swing.JFrame {
         valueLabel = new javax.swing.JTextField();
         jSeparator2 = new javax.swing.JSeparator();
         autoenviarCheckbox = new javax.swing.JCheckBox();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
+        redeButton = new javax.swing.JButton();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jLabel8 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Publisher / Sensor Adder");
@@ -51,16 +56,16 @@ public class Principal extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\vitau\\Documents\\Unifei II\\Sistemas_Distribuidos\\Publisher_Maven\\flash.png")); // NOI18N
         jLabel2.setText("Publisher");
-        jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 70, -1, 44));
+        jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 70, -1, 44));
 
         jSeparator1.setBackground(new java.awt.Color(153, 153, 153));
         jSeparator1.setForeground(new java.awt.Color(51, 255, 102));
-        jPanel3.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 130, 344, 10));
+        jPanel3.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 130, 344, 10));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Sensor ID:");
-        jPanel3.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 170, -1, -1));
+        jPanel3.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 170, -1, -1));
 
         sensorIDLabel.setBackground(new java.awt.Color(51, 0, 102));
         sensorIDLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
@@ -69,44 +74,44 @@ public class Principal extends javax.swing.JFrame {
         sensorIDLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         sensorIDLabel.setCaretColor(new java.awt.Color(0, 204, 204));
         sensorIDLabel.setDisabledTextColor(new java.awt.Color(255, 255, 255));
-        jPanel3.add(sensorIDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 170, 216, -1));
+        jPanel3.add(sensorIDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 170, 216, -1));
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Tipo:");
-        jPanel3.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 210, -1, -1));
+        jPanel3.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 210, -1, -1));
 
         tipoCombobox.setBackground(new java.awt.Color(51, 0, 102));
         tipoCombobox.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tipoCombobox.setForeground(new java.awt.Color(255, 255, 255));
         tipoCombobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Temperatura", "Presença", "Umidade" }));
         tipoCombobox.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel3.add(tipoCombobox, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 210, 216, -1));
+        jPanel3.add(tipoCombobox, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 210, 216, -1));
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Rede:");
-        jPanel3.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 250, -1, -1));
+        jPanel3.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 250, -1, -1));
 
         redeCombobox.setBackground(new java.awt.Color(51, 0, 102));
         redeCombobox.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         redeCombobox.setForeground(new java.awt.Color(255, 255, 255));
         redeCombobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Rede 1", "Rede 2" }));
-        jPanel3.add(redeCombobox, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 250, 216, -1));
+        jPanel3.add(redeCombobox, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 250, 216, -1));
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("Mensagem:");
-        jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 290, -1, -1));
+        jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 290, -1, -1));
 
         msgLabel.setBackground(new java.awt.Color(51, 0, 102));
         msgLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         msgLabel.setForeground(new java.awt.Color(255, 255, 255));
         msgLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel3.add(msgLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 290, 216, -1));
+        jPanel3.add(msgLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 290, 216, -1));
 
         sendButton.setBackground(new java.awt.Color(51, 0, 102));
-        sendButton.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        sendButton.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         sendButton.setForeground(new java.awt.Color(51, 255, 102));
         sendButton.setText("Enviar");
         sendButton.setBorder(javax.swing.BorderFactory.createEtchedBorder());
@@ -115,34 +120,93 @@ public class Principal extends javax.swing.JFrame {
                 sendButtonActionPerformed(evt);
             }
         });
-        jPanel3.add(sendButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 430, 99, -1));
+        jPanel3.add(sendButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 380, 99, -1));
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Dados:");
-        jPanel3.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 330, -1, -1));
+        jPanel3.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 330, -1, -1));
 
         valueLabel.setBackground(new java.awt.Color(51, 0, 102));
         valueLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         valueLabel.setForeground(new java.awt.Color(255, 255, 255));
         valueLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel3.add(valueLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 330, 216, -1));
+        jPanel3.add(valueLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 330, 216, -1));
 
         jSeparator2.setBackground(new java.awt.Color(153, 153, 153));
         jSeparator2.setForeground(new java.awt.Color(51, 255, 102));
-        jPanel3.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 370, 344, 10));
+        jPanel3.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 370, 344, 10));
 
         autoenviarCheckbox.setBackground(new java.awt.Color(51, 0, 102));
-        autoenviarCheckbox.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        autoenviarCheckbox.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         autoenviarCheckbox.setForeground(new java.awt.Color(255, 255, 255));
-        autoenviarCheckbox.setText("Autoenviar ");
+        autoenviarCheckbox.setText("Autogerar");
         autoenviarCheckbox.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         autoenviarCheckbox.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 autoenviarCheckboxActionPerformed(evt);
             }
         });
-        jPanel3.add(autoenviarCheckbox, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 390, -1, -1));
+        jPanel3.add(autoenviarCheckbox, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 380, -1, -1));
+
+        jPanel1.setBackground(new java.awt.Color(0, 0, 51));
+
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Pub / Sub");
+
+        redeButton.setBackground(new java.awt.Color(0, 0, 51));
+        redeButton.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        redeButton.setForeground(new java.awt.Color(51, 255, 102));
+        redeButton.setText("Publicar Rede");
+        redeButton.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        redeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                redeButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(37, 37, 37)
+                        .addComponent(jLabel7))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(redeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel7)
+                .addGap(18, 18, 18)
+                .addComponent(redeButton)
+                .addContainerGap(463, Short.MAX_VALUE))
+        );
+
+        jPanel3.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 540));
+
+        jCheckBox1.setBackground(new java.awt.Color(51, 0, 102));
+        jCheckBox1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jCheckBox1.setForeground(new java.awt.Color(255, 255, 255));
+        jCheckBox1.setText("Autoenviar");
+        jCheckBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBox1ActionPerformed(evt);
+            }
+        });
+        jPanel3.add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 380, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(51, 255, 102));
+        jLabel8.setText("© 2018, VictorTanabe");
+        jPanel3.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 520, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -152,7 +216,7 @@ public class Principal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 540, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -182,22 +246,35 @@ public class Principal extends javax.swing.JFrame {
        
     }//GEN-LAST:event_autoenviarCheckboxActionPerformed
 
+    private void redeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_redeButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_redeButtonActionPerformed
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jCheckBox1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JCheckBox autoenviarCheckbox;
+    private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JTextField msgLabel;
+    private javax.swing.JButton redeButton;
     private javax.swing.JComboBox<String> redeCombobox;
     private javax.swing.JButton sendButton;
     private javax.swing.JTextField sensorIDLabel;
