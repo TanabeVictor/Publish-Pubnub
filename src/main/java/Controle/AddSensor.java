b98ff2d0e12c780268892808a2fe4d5307770805
Tanabe <tanabebr@gmail.com>
2018-08-27 21:49:22 -0300

    Implementação de Criptografia Básica de USSID de Hospedeiro

--- src/main/java/Controle/AddSensor.java
+++ src/main/java/Controle/AddSensor.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controle;
 
 import Entidade.Sensor;
 
-/**
- *
- * @author vitau
- */
 public interface AddSensor {
     public void adicionaSensor(Sensor Sensor);
 }
--- src/main/java/Controle/Configurador.java
+++ src/main/java/Controle/Configurador.java
@@ -5,19 +5,24 @@ import com.pubnub.api.callbacks.PNCallback;
 import com.pubnub.api.models.consumer.PNPublishResult;
 import com.pubnub.api.models.consumer.PNStatus;
 import java.util.Arrays;
+import java.util.UUID;
 
 public class Configurador {
     final String PubKey = "pub-c-3701cc0b-3e9b-4f08-aad4-fb2c0a7de660";
     final String SubKey = "sub-c-bdb4e6fa-a701-11e8-b082-12b6fee23487";
-    final String UserId = "User de Publisher";
     final String kChannel = "Teste";
+    String UserId = null;
+    UUID uuid = UUID.randomUUID();
+    String aux = uuid.toString();
     PubNub pubnub;
     
     public Configurador(){
-
+        
         PNConfiguration pnConfiguration = new PNConfiguration();
         pnConfiguration.setPublishKey(PubKey);
         pnConfiguration.setSubscribeKey(SubKey);
+        //Criptografia de UserID
+        UserId = "PubNubID" + createUserID(aux);
         pnConfiguration.setUuid(UserId);
         pubnub= new PubNub(pnConfiguration);}
     
@@ -34,8 +39,15 @@ public class Configurador {
                 .withPresence() // also subscribe to related presence information
                 .execute();
                 } catch (Exception e) {
-			System.out.println("Error in configuration");
+			System.out.println("Erro na Configuração");
 			e.printStackTrace();
 		}
 	}
+
+        public int createUserID(String aux){
+        int hash = 7;
+        for (int i = 0; i < aux.length(); i++) {
+        hash = hash*31 + aux.charAt(i);}
+        return hash;}
+ 
 }         
